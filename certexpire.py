1e855dabe2142bd8e7bbce49d7e82118ac6b46e9
Helmut Grohne <h.grohne@cygnusnetworks.de>
2013-04-30 14:54:26 +0200

    implement missing features
     * Use VEVENTs instead of VTODOs.
     * Provide stable UIDs.

--- certexpire.py
+++ certexpire.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import datetime
+import hashlib
 import sys
 
 import OpenSSL.crypto
@@ -12,20 +13,27 @@ def parsedate(s):
 	return datetime.datetime.strptime(s[:-1], "%Y%m%d%H%M%S")
 
 def main():
+	seen = set()
 	cal = vobject.iCalendar()
-	advance = datetime.timedelta(weeks=1)
 	for fname in sys.argv[1:]:
 		with open(fname) as f:
 			content = f.read()
+		uid = hashlib.sha256(content).hexdigest()
+		if uid in seen:
+			continue
+		seen.add(uid)
 		x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, content)
 		expires = parsedate(x509.get_notAfter())
 		cn = x509.get_subject().commonName
 
-		vev = cal.add("vtodo")
+		vev = cal.add("vevent")
+		vev.add("uid").value = uid
 		vev.add("summary").value = "Renew certificate %s" % cn
-		vev.add("status").value = "NEEDS-ACTION"
-		vev.add("due").value = expires
-		vev.add("dtstart").value = expires - advance
+		vev.add("dtend").value = expires
+		vev.add("dtstart").value = expires
+		alarm = vev.add("valarm")
+		alarm.add("uid").value = "%s-alarm" % uid
+		alarm.add("trigger").value = -datetime.timedelta(weeks=3)
 	sys.stdout.write(cal.serialize())
 
 if __name__ == "__main__":
