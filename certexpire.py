f27720043021e726ea93b0497613ef913c93a9dd
Helmut Grohne <h.grohne@cygnusnetworks.de>
2013-06-14 16:19:55 +0200

    provide options
     * Use argparse to provide --help.
     * Process directories.
     * Provide a means to specify an output file.

--- certexpire.py
+++ certexpire.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
+import argparse
 import datetime
 import hashlib
+import os
 import sys
 
 import OpenSSL.crypto
@@ -12,29 +14,43 @@ def parsedate(s):
 		raise ValueError("timezone parsing not implemented")
 	return datetime.datetime.strptime(s[:-1], "%Y%m%d%H%M%S")
 
+def process_cert(cal, seen, filename):
+	with open(filename) as f:
+		content = f.read()
+	uid = hashlib.sha256(content).hexdigest()
+	if uid in seen:
+		return
+	seen.add(uid)
+	x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, content)
+	expires = parsedate(x509.get_notAfter())
+	cn = x509.get_subject().commonName
+
+	vev = cal.add("vevent")
+	vev.add("uid").value = uid
+	vev.add("summary").value = "Renew certificate %s" % cn
+	vev.add("dtend").value = expires
+	vev.add("dtstart").value = expires
+	alarm = vev.add("valarm")
+	alarm.add("uid").value = "%s-alarm" % uid
+	alarm.add("trigger").value = -datetime.timedelta(weeks=3)
+
 def main():
+	parser = argparse.ArgumentParser(description='certificate expiry to ical converter')
+	parser.add_argument('certificates', metavar="CERTIFICATE", type=str, nargs="+", help="file or directory name to process")
+	parser.add_argument('-o', '--output', metavar='FILENAME', type=argparse.FileType("w"), default=sys.stdout, help='store generated ical in given FILENAME instead of stdout')
+	args = parser.parse_args()
+
+
 	seen = set()
 	cal = vobject.iCalendar()
-	for fname in sys.argv[1:]:
-		with open(fname) as f:
-			content = f.read()
-		uid = hashlib.sha256(content).hexdigest()
-		if uid in seen:
-			continue
-		seen.add(uid)
-		x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, content)
-		expires = parsedate(x509.get_notAfter())
-		cn = x509.get_subject().commonName
+	for fname in args.certificates:
+		if os.path.isdir(fname):
+			for entry in os.listdir(fname):
+				process_cert(cal, seen, os.path.join(fname, entry))
+		else:
+			process_cert(cal, seen, fname)
 
-		vev = cal.add("vevent")
-		vev.add("uid").value = uid
-		vev.add("summary").value = "Renew certificate %s" % cn
-		vev.add("dtend").value = expires
-		vev.add("dtstart").value = expires
-		alarm = vev.add("valarm")
-		alarm.add("uid").value = "%s-alarm" % uid
-		alarm.add("trigger").value = -datetime.timedelta(weeks=3)
-	sys.stdout.write(cal.serialize())
+	args.output.write(cal.serialize())
 
 if __name__ == "__main__":
 	main()
