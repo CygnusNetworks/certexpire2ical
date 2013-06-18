a4bbcc56747c48629beaf61c1aa0eee8eb4d306f
Helmut Grohne <h.grohne@cygnusnetworks.de>
2013-06-18 11:24:57 +0200

    use correct log levels
     * Use the log levels according to python-coding-guidelines_001.pdf
     * Make multiple -v options increase log level.

--- certexpire.py
+++ certexpire.py
@@ -28,19 +28,19 @@ class Cert2Cal(object):
 		self.log = log
 
 	def add_cert(self, filename):
-		self.log.log_info("Processing file %s" % (filename,))
+		self.log.log_debug("Processing file %s" % (filename,), 1)
 		with open(filename) as f:
 			content = f.read()
 		uid = hashlib.sha256(content).hexdigest()
 		if uid in self.seen:
-			self.log.log_info("Skipping file %s. Allready processed a different file with hash %s." % (filename, uid))
+			self.log.log_debug("Skipping file %s. Allready processed a different file with hash %s." % (filename, uid), 2)
 			return
-		self.log.log_debug("Computed hash %s fr file %s." % (uid, filename))
+		self.log.log_debug("Computed hash %s fr file %s." % (uid, filename), 3)
 		self.seen.add(uid)
 		x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, content)
 		expires = parsedate(x509.get_notAfter())
 		cn = x509.get_subject().commonName
-		self.log.log_debug("Determined common name for %s as %r and expireation as %s." % (filename, cn, expires))
+		self.log.log_debug("Determined common name for %s as %r and expireation as %s." % (filename, cn, expires), 3)
 
 		vev = self.cal.add("vevent")
 		vev.add("uid").value = uid
@@ -62,15 +62,15 @@ def main():
 	parser.add_argument('certificates', metavar="CERTIFICATE", type=str, nargs="+", help="file or directory name to process")
 	parser.add_argument('-o', '--output', metavar='FILENAME', type=argparse.FileType("w"), default=sys.stdout, help='store generated ical in given FILENAME instead of stdout')
 	parser.add_argument('-d', '--duration', metavar='DAYS', type=days, default=days(21), help="how much time before expiry should the events trigger")
-	parser.add_argument('-v', '--verbose', action='store_true', help='print debugging output')
+	parser.add_argument('-v', '--verbose', action='count', help='increase debug level')
 	args = parser.parse_args()
 
-	log = SysloggingDebugLevel("certexpire", quiet=not args.verbose, verbosefile=sys.stderr)
+	log = SysloggingDebugLevel("certexpire", quiet=False, verbosefile=sys.stderr, log_level=args.verbose)
 
 	cert2cal = Cert2Cal(args.duration, log)
 	for fname in args.certificates:
 		if os.path.isdir(fname):
-			log.log_info("Processing directory %s" % (fname,))
+			log.log_debug("Processing directory %s" % (fname,), 0)
 			for entry in os.listdir(fname):
 				cert2cal.add_cert(os.path.join(fname, entry))
 		else:
