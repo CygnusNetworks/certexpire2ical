fe1911a2e2726567b19228c55ce61ec84642c1c1
Dr. Torge Szczepanek <info@cygnusnetworks.de>
2019-01-15 16:29:38 +0100

    Make Python3 compatible

--- certexpire2ical.py
+++ certexpire2ical.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-__author__		= "Helmut Grohne"
-__copyright__	= "Cygnus Networks GmbH"
-__license__		= "proprietary"
-__version__		= "0.1"
-__maintainer__	= "Cygnus Networks GmbH"
-__email__		= "info@cygnusnetworks.de"
-__status__		= "Development"
 
 import argparse
 import datetime
@@ -19,20 +11,27 @@ import sys
 import OpenSSL.crypto
 import vobject
 
+__author__ = "Helmut Grohne"
+__copyright__ = "Cygnus Networks GmbH"
+__license__ = "proprietary"
+__version__ = "0.2"
+__maintainer__ = "Cygnus Networks GmbH"
+__email__ = "info@cygnusnetworks.de"
+__status__ = "Beta"
+
+
 class CertNotAdded(Exception):
 	pass
+
 
 def parsedate(s):
 	if not s.endswith("Z"):
 		raise ValueError("timezone parsing not implemented")
 	return datetime.datetime.strptime(s[:-1], "%Y%m%d%H%M%S")
 
+
 class Cert2Cal(object):  # pylint:disable=R0903
 	def __init__(self, notify, expire):
-		"""
-		@type trigger_delta: datetime.timedelta
-		@param trigger_delta: how much time before expiry should the event trigger?
-		"""
 		self.cal = vobject.iCalendar()
 		self.seen = set()
 		self.notify = notify
@@ -45,7 +44,7 @@ class Cert2Cal(object):  # pylint:disable=R0903
 				content = f.read()
 		except IOError as exc:
 			raise CertNotAdded("failed to read %s: %s" % (filename, exc))
-		uid = hashlib.sha256(content).hexdigest()  # pylint:disable=E1101
+		uid = hashlib.sha256(content.encode('utf-8')).hexdigest()  # pylint:disable=E1101
 		if uid in self.seen:
 			return
 		self.seen.add(uid)
@@ -53,14 +52,14 @@ class Cert2Cal(object):  # pylint:disable=R0903
 			x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, content)
 		except OpenSSL.crypto.Error as err:
 			raise CertNotAdded("failed to parse %s with openssl: %r" % (filename, err))
-		expires = parsedate(x509.get_notAfter())
+		expires = parsedate(x509.get_notAfter().decode('ascii'))
 		cn = x509.get_subject().commonName
 
-		if expires>self.now:
+		if expires > self.now:
 			vev = self.cal.add("vevent")
 			vev.add("uid").value = uid
-                        vev.add("summary").value = "Expire CN:%s" % cn
-                        vev.add("description").value = "Filename %s" % filename
+			vev.add("summary").value = "Expire CN:%s" % cn
+			vev.add("description").value = "Filename %s" % filename
 			vev.add("dtend").value = expires
 			vev.add("dtstart").value = expires
 			alarm = vev.add("valarm")
@@ -75,8 +74,10 @@ class Cert2Cal(object):  # pylint:disable=R0903
 	def __str__(self):
 		return self.cal.serialize()
 
+
 def days(s):
 	return datetime.timedelta(days=int(s))
+
 
 def main():
 	parser = argparse.ArgumentParser(description='certificate expiry to ical converter')
@@ -102,14 +103,15 @@ def main():
 					try:
 						cert2cal.add_cert(filename)
 					except CertNotAdded as err:
-						print str(err)
+						print(err)
 		else:
 			try:
 				cert2cal.add_cert(fname)
 			except CertNotAdded as err:
-				print str(err)
+				print(err)
 
 	args.output.write(str(cert2cal))
+
 
 if __name__ == "__main__":
 	main()
